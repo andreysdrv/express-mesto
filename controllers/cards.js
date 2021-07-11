aa16ab2198e3cb2aea9a557e5a8230d4114a5f15
Andrew Sidorov <jeck666@mail.ru>
2021-07-11 12:24:16 +0300

    fix: добавить обработку ошики 400 при удалении карточки

--- controllers/cards.js
+++ controllers/cards.js
@@ -21,7 +21,13 @@ const deleteCard = (req, res) => {
         res.status(ERR_BAD_REQUEST).send({ message: 'Недостаточно прав' });
       }
     })
-    .catch(() => res.status(ERR_DEFAULT).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при удалении карточки' });
+      } else {
+        res.status(ERR_DEFAULT).send({ message: 'Произошла ошибка' });
+      }
+    });
 };
 
 const createCard = (req, res) => {
